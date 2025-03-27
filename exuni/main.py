ad641d01c3c459c679d48a2df991889e3ef7b0e0
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-03-27 15:03:20 +0100

    fix relative imports

--- exuni/main.py
+++ exuni/main.py
@@ -1,7 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
-import exuni.database as database
-import exuni.queries as queries
+import database
+import queries
 import argparse
 from pathlib import Path
 
