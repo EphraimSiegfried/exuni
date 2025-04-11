c4c0e1f5ce0dc45b566e3d47e341956b79d0bbd7
Ephraim Siegfried <ephraim.siegfried@hotmail.com>
2025-04-11 08:46:55 +0200

    add feature: calculate exercise points

--- exuni/database.py
+++ exuni/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import string
 from sqlite3 import Connection
 from gspread import Worksheet
 from datetime import datetime
@@ -72,3 +73,61 @@ def insert_contribution_data(worksheet: Worksheet, conn: Connection, exercise_id
 
     conn.commit()
 
+def insert_points_data(worksheet: Worksheet, conn: Connection):
+    cursor = conn.cursor()
+    header = worksheet.row_values(3)
+    first_col = worksheet.col_values(1)
+    num_groups = max(int(v) for v in first_col if v.isnumeric()) # kind of ugly solution
+    exercise_id = int(worksheet.acell("A1").value[-1])
+    num_tasks = len([h for h in header if "Task" in h and "Points" in h])
+    num_cols = 1 + 2 * (num_tasks + 1) + 1
+    col_id = string.ascii_uppercase[num_cols-1]
+    data_row_start = 4
+    data = worksheet.get(f"A{data_row_start}:{col_id}{data_row_start - 1 + num_groups}", maintain_size=True)
+    task_ids = list(range(1, num_tasks + 1))
+
+    for task_id in task_ids:
+        cursor.execute('''
+        INSERT OR IGNORE INTO Task (task_id, exercise_num)
+        VALUES (?, ?)
+        ''', (task_id, exercise_id))
+
+    for row in data:
+        group_id = int(row[0])
+        points = row[1:num_tasks+1]
+        points_notes = row[num_tasks+3: 3 + 2 * num_tasks]
+        malus = row[num_tasks+1]
+        malus_notes = row[3 + 2 * num_tasks]
+        for task_id, point, note in zip(task_ids, points, points_notes):
+            cursor.execute('''
+            INSERT OR IGNORE INTO TaskResult (exercise_num, task_id, group_id, points_awarded, notes)
+            VALUES (?, ?, ?, ?, ?)''', (exercise_id, task_id, group_id, point, note))
+        if not malus or int(malus) == 0:
+            continue
+        cursor.execute('''
+        INSERT OR IGNORE INTO GroupMalus (exercise_num, group_id, points_deducted, notes)
+        ''', (exercise_id, group_id, malus, malus_notes))
+    conn.commit()
+
+def insert_person_malus_data(worksheet: Worksheet, conn: Connection):
+    cursor = conn.cursor()
+    header_row = 27
+    header = worksheet.row_values(header_row)
+    exercise_id = int(worksheet.acell("A1").value[-1])
+    num_members = (len(header) - 1) // 2
+    first_col = worksheet.col_values(1)
+    num_groups = max(int(v) for v in first_col if v.isnumeric()) # kind of ugly solution
+    col_end_id = string.ascii_uppercase[len(header)-1]
+    data = worksheet.get(f"A{header_row+1}:{col_end_id}{header_row + num_groups}", maintain_size=True)
+    member_ids = string.ascii_uppercase[:num_members]
+    for row in data:
+        team_id = int(row[0])
+        malus_points = row[1:num_members+1]
+        malus_notes = row[num_members+1:]
+        for member_id, point, note in zip(member_ids, malus_points, malus_notes):
+            if not point:
+                continue
+            cursor.execute('''
+            INSERT OR IGNORE INTO PersonMalus (exercise_num, group_id, member_id, points_deducted, notes)
+            VALUES (?, ?, ?, ?, ?)''', (exercise_id, team_id, member_id, int(point), note))
+    conn.commit()
--- exuni/schema.sql
+++ exuni/schema.sql
@@ -16,14 +16,54 @@ CREATE TABLE Exercise (
 );
 
 CREATE TABLE Feedback (
+                          exercise_num INTEGER,
+                          group_id INTEGER,
+                          reviewer_id VARCHAR(1),
+                          reviewee_id VARCHAR(1),
+                          score FLOAT NOT NULL,
+                          created_at TIMESTAMP NOT NULL,
+                          PRIMARY KEY (exercise_num, group_id, reviewer_id, reviewee_id),
+                          FOREIGN KEY (exercise_num) REFERENCES Exercise(exercise_num),
+                          FOREIGN KEY (group_id, reviewer_id) REFERENCES Person(group_id, member_id),
+                          FOREIGN KEY (group_id, reviewee_id) REFERENCES Person(group_id, member_id)
+);
+
+CREATE TABLE Task (
+    task_id INTEGER,
+    exercise_num INTEGER,
+    PRIMARY KEY (exercise_num, task_id),
+    FOREIGN KEY (exercise_num) REFERENCES Exercise(exercise_num)
+);
+
+CREATE TABLE TaskResult (
+    exercise_num INTEGER,
+    task_id INTEGER,
+    group_id INTEGER,
+    points_awarded FLOAT NOT NULL,
+    notes TEXT,
+    PRIMARY KEY (exercise_num, task_id, group_id),
+    FOREIGN KEY (exercise_num, task_id) REFERENCES Task(exercise_num, task_id),
+    FOREIGN KEY (group_id) REFERENCES "Group"(group_id)
+);
+
+CREATE TABLE GroupMalus (
     exercise_num INTEGER,
     group_id INTEGER,
-    reviewer_id VARCHAR(1),
-    reviewee_id VARCHAR(1),
-    score FLOAT NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    PRIMARY KEY (exercise_num, group_id, reviewer_id, reviewee_id),
+    points_deducted FLOAT NOT NULL,
+    notes TEXT,
+    PRIMARY KEY (exercise_num, group_id),
     FOREIGN KEY (exercise_num) REFERENCES Exercise(exercise_num),
-    FOREIGN KEY (group_id, reviewer_id) REFERENCES Person(group_id, member_id),
-    FOREIGN KEY (group_id, reviewee_id) REFERENCES Person(group_id, member_id)
+    FOREIGN KEY (group_id) REFERENCES "Group"(group_id)
 );
+
+CREATE TABLE PersonMalus (
+    exercise_num INTEGER,
+    group_id INTEGER,
+    member_id VARCHAR(1),
+    points_deducted FLOAT NOT NULL,
+    notes TEXT,
+    PRIMARY KEY (exercise_num, group_id, member_id),
+    FOREIGN KEY (exercise_num) REFERENCES Exercise(exercise_num),
+    FOREIGN KEY (group_id, member_id) REFERENCES Person(group_id, member_id)
+);
+
